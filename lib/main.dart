0c02eac94653a7620fe46ddd76807b2fd5505c16
Richard Muragijimana <beastar457@gmail.com>
2021-07-09 14:17:30 +0200

    get the package initial

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flipper_chat/pages/root_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: RootApp(),
+  ));
+}
---
+++ packages/flipper_chat/lib/flipper_chat.dart
@@ -0,0 +1,7 @@
+library flipper_chat;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}

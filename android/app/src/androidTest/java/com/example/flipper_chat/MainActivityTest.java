7d551ee5d983b44c7573e256f9809a114a0a1ed2
Richard Muragijimana <beastar457@gmail.com>
2021-07-11 14:00:08 +0200

    main

--- android/app/src/androidTest/java/com/example/flipper_chat/MainActivityTest.java
+++ android/app/src/androidTest/java/com/example/flipper_chat/MainActivityTest.java
@@ -5,7 +5,7 @@ import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 // Import MainActivity from YOUR package
-import com.resocoder.integration_testing_prep.MainActivity;
+// import com.example.flipper_chat.MainActivity;
 
 @RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
